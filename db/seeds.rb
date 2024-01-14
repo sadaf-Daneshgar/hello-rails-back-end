a513eb31044c2c1310645e60c45fe62f3e0b2e56
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-14 22:21:42 +0430

    create the message table with greeting messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+greetings = [
+  "Hello, world!",
+  "Greetings from Rails!",
+  "Welcome to the API",
+  "Hola amigos!",
+  "Howdy, partner!"
+]
+
+greetings.each do |greeting|
+    Message.create(content: greeting)
+end
