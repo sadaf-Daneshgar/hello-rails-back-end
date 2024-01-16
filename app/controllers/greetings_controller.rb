9f7cedc217d3feeb55c86ca50eab7b50c0f309c3
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-16 10:30:34 +0430

    add the greetings controller

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def random
     random_message = Message.order('RANDOM()').first
-    render json: { greeting: random_message&.content || 'No greetings available' }
+    render json: { greeting: random_message&.content }
   end
 end
